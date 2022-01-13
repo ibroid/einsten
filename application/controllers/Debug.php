486359404353b2a92b08e874b22c11c3393ecaf3
ibroid <maulana858@gmail.com>
2022-01-12 20:45:08 -0500

    init before pull

--- application/controllers/Debug.php
+++ application/controllers/Debug.php
@@ -12,13 +12,13 @@ class Debug extends CI_Controller
     {
         $data = $this->capsule->table('jurusita')->where('aktif', 'Y')->get();
         foreach ($data as $key => $value) {
-            // $this->capsule->connection('local')->table('users')->insert([
-            //     'username' => $value->keterangan,
-            //     'password' => password_hash('paju', PASSWORD_DEFAULT),
-            //     'profile_id' => $value->id,
-            //     'level_id' => 6,
-            //     'created_at' => date('Y-m-d H:i:s')
-            // ]);
+            $this->capsule->connection('local')->table('users')->insert([
+                'username' => $value->keterangan,
+                'password' => password_hash('paju', PASSWORD_DEFAULT),
+                'profile_id' => $value->id,
+                'level_id' => 7,
+                'created_at' => date('Y-m-d H:i:s')
+            ]);
             echo $value->nama . '<br>';
         }
     }
