2354d0ebd3f9e33a704ada70c924e8631878f873
ibroid <maulana858@gmail.com>
2024-10-23 09:06:11 +0700

    notif ok

--- application/models/Instrumens.php
+++ application/models/Instrumens.php
@@ -9,6 +9,20 @@ class Instrumens extends Illuminate\Database\Eloquent\Model
     protected static function boot()
     {
         parent::boot();
+
+        static::created(function ($data) {
+            $ci = &get_instance();
+            $tglsidang = tanggal_indo($data->untuk_tanggal);
+
+            $message = "*INSTRUMEN BARU*. Jenis Panggilan: $data->jenis_panggilan.\nNomor Perkara : " . $data->perkara->nomor_perkara . ".\nPihak : " . $data->pihak->nama . ".\nAlamat:" . $data->pihak->alamat . ".\nJenis Pihak : $data->jenis_pihak. Tanggal Sidang/Putus:$tglsidang";
+
+            $ci->load->library('wanotif', [
+                'number' => $data->jurusita->keterangan,
+                'text' => $message
+            ]);
+
+            $ci->wanotif->send();
+        });
     }
 
     public function perkara()
